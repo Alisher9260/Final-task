1f5049c5b964ff58af595870b46aa8f55cccc4d3
Alisher9260 <111871619+Alisher9260@users.noreply.github.com>
2022-10-04 19:23:50 +0800

    Add files via upload
    добавление кода программы

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Какое количество элементов необходимо ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] newarray = new string[size];
+int sym = 3;
+int num = 0;
+for (int i = 0; i < size; i++)
+{
+ Console.WriteLine($"Введите {i+1}-й элемент: ");
+ string element = Convert.ToString(Console.ReadLine());
+ if (element.Length <= sym)
+    {
+ newarray[num] = element;
+ num++;
+    }
+}
+Console.WriteLine();
+ShowArray(newarray);
